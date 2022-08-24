097cde5208f0f43f7247ad8bd53e15a2e7d996c9
bondyuu <leo4902@gmail.com>
2022-08-24 14:54:24 +0900

    profilecontroller 수정

--- clone/src/main/java/com/sparta/clone/controller/ProfileController.java
+++ clone/src/main/java/com/sparta/clone/controller/ProfileController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class ProfileController {
     private final Environment env;
 
-    @GetMapping
+    @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real1", "real2");
