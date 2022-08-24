1701008b6359f3747fffa79abab6651feb283026
bondyuu <leo4902@gmail.com>
2022-08-24 20:40:00 +0900

    Update deploy.sh

--- clone/scripts/deploy.sh
+++ clone/scripts/deploy.sh
@@ -29,5 +29,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -Dspring.config.location=/home/ubuntu/application.yml -jar $JAR_NAME
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -Dspring.config.location=/home/ubuntu/application.yml -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
